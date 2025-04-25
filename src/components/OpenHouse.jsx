ec9c7ced6eae4e26b7f544c67c4660330eff4274
shivam-nitt <skm734786@gmail.com>
2025-04-25 08:25:36 +0530

    updated openhouse page

--- src/components/OpenHouse.jsx
+++ src/components/OpenHouse.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import GalleryPage from "./GalleryPage";
-import Openhouse1 from "../assets/Gallery/Openhouse/OH1.JPG";
+
 import Openhouse2 from "../assets/Gallery/Openhouse/OH2.JPG";
 import Openhouse3 from "../assets/Gallery/Openhouse/OH3.JPG";
 import Openhouse4 from "../assets/Gallery/Openhouse/OH4.JPG";
@@ -8,6 +8,13 @@ import Openhouse5 from "../assets/Gallery/Openhouse/OH5.JPG";
 import Openhouse6 from "../assets/Gallery/Openhouse/OH6.JPG";
 import Openhouse7 from "../assets/Gallery/Openhouse/OH7.JPG";
 import Openhouse8 from "../assets/Gallery/Openhouse/OH8.JPG";
+import Openhouse9 from "../assets/Gallery/Openhouse/OH9.JPG";
+import Openhouse10 from "../assets/Gallery/Openhouse/OH10.jpg";
+import Openhouse11 from "../assets/Gallery/Openhouse/OH11.JPG";
+import Openhouse12 from "../assets/Gallery/Openhouse/OH12.jpg";
+import Openhouse13 from "../assets/Gallery/Openhouse/OH13.jpg";
+import Openhouse14 from "../assets/Gallery/Openhouse/OH14.jpg";
+import Openhouse1 from "../assets/Gallery/Openhouse/OH1.png";
 
 const images = [
   Openhouse1,
@@ -18,6 +25,12 @@ const images = [
   Openhouse6,
   Openhouse7,
   Openhouse8,
+  Openhouse9,
+  Openhouse10,
+  Openhouse11,
+  Openhouse12,
+  Openhouse13,
+  Openhouse14
 ];
 
 export default function OpenHouse() {
