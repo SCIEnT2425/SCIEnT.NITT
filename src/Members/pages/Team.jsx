4d23e8e1fa1820b43939dfd1479a779e83b5248d
Div-agr <divyanshkumaragrawal80@gmail.com>
2025-11-05 14:57:04 +0530

    Team page reveal

--- src/Members/pages/Team.jsx
+++ src/Members/pages/Team.jsx
@@ -12,12 +12,7 @@ const Team = () => {
       <nav className='clear-both'>
         <Navbar/>
       </nav>
-      <div className='w-screen h-screen text-black bg-orange-500 clear-both'>
-        <h1>
-        This section is for group picture of scient
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quo quasi modi officiis quas velit aliquid saepe, architecto blanditiis excepturi nesciunt in doloribus, cupiditate a obcaecati nobis autem enim quis cumque alias eum impedit cum optio ex. Veritatis, maiores ipsum dolorem dolore cum, natus facere aut, error recusandae perferendis odio! Quis quasi odio aliquam quod deleniti.
-        </h1>
-      </div>
+      <ScientMembers/>
       <footer className='w-screen clear-both'>
         <Footer/>
       </footer>
