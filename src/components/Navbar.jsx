4dafa776e67719be5a5a3652bab387b56ca013ff
sekhar9454 <sharmasanskar7201@gmail.com>
2026-03-03 22:34:08 +0530

    Added Faculty Connect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import { Link, useLocation } from 'react-router-dom';
 import "./Navbar.css";
 import logo from "../assets/logo_s.png";
 import search from "../assets/search.svg";
-import {ChevronDown} from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
-const Navbar = () => { 
+const Navbar = () => {
     const location = useLocation();
     const [clicked, setClicked] = useState(false);
 
     const handleClick = () => {
-        setClicked(clicked=>!clicked); 
+        setClicked(clicked => !clicked);
         console.log(clicked)
     };
     const handleClick2 = () => {
         setClicked(clicked => false);
-        
+
     };
 
     // Helper function to check if a link is active
@@ -24,9 +24,9 @@ const Navbar = () => {
     return (
         <div>
             <nav className="nav py-2 relative">
-                <Link to="/"  className="mr-10"
+                <Link to="/" className="mr-10"
                     onClick={handleClick2}>
-                    <img  className="logo" src={logo} alt="Logo" />
+                    <img className="logo" src={logo} alt="Logo" />
                 </Link>
 
                 <div className={`links ${!clicked ? 'active1 text-2xl' : ''}`}>
@@ -53,7 +53,7 @@ const Navbar = () => {
                     >
                         Inventory
                     </Link>
-                         <Link
+                    <Link
                         to="/openhouse"
                         onClick={handleClick}
                         className={`nav-link ${isActive('/openhouse') ? 'active font-bold text-2xl' : ''}`}
@@ -86,31 +86,33 @@ const Navbar = () => {
                         <div className="absolute left-0 hidden w-150 rounded-lg bg-black shadow group-hover:block text-center border">
                             <div className="cursor-pointer px-4 py-2 hover:bg-gray-500 whitespace-nowrap">
                                 <Link to="/inventive"
-                                      onClick={handleClick}
-                                      className={`nav-link ${isActive('/inventive') ? 'active font-bold text-2xl' : ''} `}
+                                    onClick={handleClick}
+                                    className={`nav-link ${isActive('/inventive') ? 'active font-bold text-2xl' : ''} `}
                                 >
                                     Inventive '25
                                 </Link>
                             </div>
                             <div className="cursor-pointer px-4 py-2 hover:bg-gray-500 whitespace-nowrap">
                                 <Link to="/contrive"
-                                      onClick={handleClick}
-                                      className={`nav-link ${isActive('/contrive') ? 'active font-bold text-2xl' : ''} `}
+                                    onClick={handleClick}
+                                    className={`nav-link ${isActive('/contrive') ? 'active font-bold text-2xl' : ''} `}
                                 >
                                     Contrive '25
                                 </Link>
                             </div>
                         </div>
                     </div>
-                   
-                    <Link
-                        to="/faculty-connect"
+
+                    <a
+                        href="https://faculty-connect-1.onrender.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
                         onClick={handleClick}
-                        className={`nav-link ${isActive('/faculty-connect') ? 'active font-bold text-2xl' : ''} `}
+                        className={`nav-link ${isActive('/faculty-connect') ? 'active font-bold text-2xl' : ''}`}
                     >
                         Faculty Connect
-                    </Link>
-                   
+                    </a>
+
                     <div className="relative inline-block group">
                         <div className="cursor-pointer rounded-md px-4 py-2 flex items-center">
                             <div className="mr-2">About Us</div>
@@ -120,24 +122,24 @@ const Navbar = () => {
                         <div className="absolute left-0 hidden w-100 rounded-lg bg-black shadow group-hover:block">
                             <div className="cursor-pointer px-4 py-2 hover:bg-gray-500">
                                 <Link to="/timeline"
-                                      onClick={handleClick}
-                                      className={`nav-link ${isActive('/timeline') ? 'active font-bold text-2xl' : ''} `}
+                                    onClick={handleClick}
+                                    className={`nav-link ${isActive('/timeline') ? 'active font-bold text-2xl' : ''} `}
                                 >
                                     Timeline
                                 </Link>
                             </div>
                             <div className="cursor-pointer px-4 py-2 hover:bg-gray-500">
                                 <Link to="/team"
-                                      onClick={handleClick}
-                                      className={`nav-link ${isActive('/team') ? 'active font-bold text-2xl' : ''} `}
+                                    onClick={handleClick}
+                                    className={`nav-link ${isActive('/team') ? 'active font-bold text-2xl' : ''} `}
                                 >
                                     Team
                                 </Link>
                             </div>
                             <div className="cursor-pointer px-4 py-2 hover:bg-gray-500">
                                 <Link to="/gallery"
-                                      onClick={handleClick}
-                                      className={`nav-link ${isActive('/gallery') ? 'active font-bold text-2xl' : ''} `}
+                                    onClick={handleClick}
+                                    className={`nav-link ${isActive('/gallery') ? 'active font-bold text-2xl' : ''} `}
                                 >
                                     Gallery
                                 </Link>
@@ -146,8 +148,8 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div id="mobile" onClick={handleClick}>
-                     <i id="bar" className={clicked ? 'fas fa-times':'fas fa-bars'}></i>
-                </div> 
+                    <i id="bar" className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
             </nav>
         </div>
     );
